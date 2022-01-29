71e2f61f9c4055746086c8324c84a93a33b84885
Sorokin Ivan <iso@goldcarrot.ru>
2022-01-29 14:30:28 +0300

    (add) languages files

---
+++ Configs/languages/english.py
@@ -0,0 +1,9 @@
+translations = {
+    'start': '/start works!!!',
+    'help': '/help works!!!',
+    'number-value': "Number value: {value}",
+    'letter-value': "Letter value: {value}",
+    'hello': 'Hello!!!',
+    'joke': '*Funny joke*',
+    'dont-understand': "Sorry, i don't understand, type /start or /help"
+}
--- Handlers/example.py
+++ Handlers/example.py
@@ -2,51 +2,50 @@ from aiogram import types
 from aiogram.dispatcher.filters import Text
 
 from Settings import settings
+from Configs import translations
 from Services import ExampleService
 from Callbacks import example_callback
 from Keyboards import example_keyboard
-
-# ! TODO: add languages file
 
 
 # <<<<<<<<<<<<<<<<<< Command [answering with keyboard] >>>>>>>>>>>>>>>>>>
 @settings.dp.message_handler(commands=["start"])
 async def command_example(message: types.Message):
-    await message.answer("/start works!!!", reply_markup=example_keyboard.main)
+    await message.answer(translations.get('start'), reply_markup=example_keyboard.main)
 
 
 # <<<<<<<<<<<<<<<<<< Command with callback >>>>>>>>>>>>>>>>>>
 @settings.dp.message_handler(commands=["help"])
 async def command_example(message: types.Message):
     inline = ExampleService.get_example_inline_callback()
-    await message.answer("/help works!!!", reply_markup=inline)
+    await message.answer(translations.get('help'), reply_markup=inline)
 
 
 # <<<<<<<<<<<<<<<<<< Callback action with [filtering by type=number] >>>>>>>>>>>>>>>>>>
 @settings.dp.callback_query_handler(example_callback.example_callback_data.filter(type="number"))
 async def callback_number_example(call: types.CallbackQuery, callback_data: dict):
-    await call.message.edit_text(f"Number value: {callback_data['value']}")
+    await call.message.edit_text(translations.get('number-value').format(value=callback_data['value']))
 
 
 # <<<<<<<<<<<<<<<<<< Callback action with [filtering by type=letter] >>>>>>>>>>>>>>>>>>
 @settings.dp.callback_query_handler(example_callback.example_callback_data.filter(type="letter"))
 async def callback_letter_example(call: types.CallbackQuery, callback_data: dict):
-    await call.message.edit_text(f"Letter value: {callback_data['value']}")
+    await call.message.edit_text(translations.get('letter-value').format(value=callback_data['value']))
 
 
 # <<<<<<<<<<<<<<<<<< Message with filters by many words >>>>>>>>>>>>>>>>>>
 @settings.dp.message_handler(Text(equals=['hi', 'hello'], ignore_case=True))
 async def hello_message_example(message: types.Message):
-    await message.answer("Hello!!!")
+    await message.answer(translations.get('hello'))
 
 
 # <<<<<<<<<<<<<<<<<< Message with filters by one word >>>>>>>>>>>>>>>>>>
 @settings.dp.message_handler(Text(equals='joke', ignore_case=True))
 async def joke_message_example(message: types.Message):
-    await message.answer("<<Funny joke>>")
+    await message.answer(translations.get('joke'))
 
 
 # <<<<<<<<<<<<<<<<<< Any another message >>>>>>>>>>>>>>>>>>
 @settings.dp.message_handler()
 async def any_message_handler_example(message: types.Message):
-    await message.answer("Sorry, i don't understand, type /start or /help")
+    await message.answer(translations.get('dont-understand'))
